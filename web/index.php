6bfe894059e93634e01aac0ad0f52a44c07f6a23
Aidan Jacobson <survivalking4@gmail.com>
2020-06-03 00:46:09 -0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,3 +1,19 @@
 <?php
-echo "hi";
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    
+    header("Content-Type: application/json");
+
+    $test_username = "test1234";
+
+    $obj = (object)[];
+    $obj->accessToken = "1234";
+    $obj->clientToken = "5678";
+    $s = (object)[];
+    $s->agent = "minecraft";
+    $s->id="";
+    $s->name=$test_username;
+    $obj->availableProfiles = [$s];
+    $obj->selectedProfile = $s;
+    echo json_encode($obj);
 ?>
